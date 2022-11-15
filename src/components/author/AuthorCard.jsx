37dd47ba7038a3a8da296c45145f39c868c76c79
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-11-15 16:05:08 +0530

    author mobile update

--- src/components/author/AuthorCard.jsx
+++ src/components/author/AuthorCard.jsx
@@ -10,14 +10,14 @@ import nadiaUsman from '../../assets/images/nadia-usman.png';
 const AuthorCard = () => {
     return (
         <div>
-            <div className="p-4 rounded-md border bg-white my-3 w-[837px]">
+            <div className="p-4 rounded-md border bg-white my-3 w-[95vw] sm:w-[837px]">
                 <div className="flex gap-x-4">
-                    <div className="overflow-hidden rounded-full h-[110px]">
-                        <img className="" src={nadiaUsman} alt="" />
+                    <div className="overflow-hidden rounded-full h-[70px] w-[287px]">
+                        <img className="h-[100%] w-[100%]" src={nadiaUsman} alt="" />
                     </div>
                     <div>
-                        <h1 className="font-[800] text-[20px] font-nunito">Nadia Usman</h1>
-                        <p className='text-[#6d6d6d] text-sm font-[400] font-nunito'>Well, a Content Writer, and Designer by Profession and Marvel & DC Comics Nerd by Passion, Completed BSc. Graphic and animation from MCU, Bhopal</p>
+                        <h1 className="font-[800] text-[20px] font-nunitoSans">Nadia Usman</h1>
+                        <p className='text-[#6d6d6d] text-sm font-[400] font-nunitoSans'>Well, a Content Writer, and Designer by Profession and Marvel & DC Comics Nerd by Passion, Completed BSc. Graphic and animation from MCU, Bhopal</p>
                         <div className="flex gap-x-3 text-xl mt-4">
 
                             <img height={25} width={25} src={instagramIcon} alt="" />
--- src/components/author/AuthorContainer.jsx
+++ src/components/author/AuthorContainer.jsx
@@ -7,14 +7,14 @@ import AuthorCard from './AuthorCard'
 
 const AuthorContainer = () => {
   return (
-    <div className=" mt-10  flex justify-center items-center">
-      <div className="px-2  w-[1264px]">
+    <div className="mt-6 sm:mt-10  flex justify-center items-center">
+      <div className="sm:px-2 w-[95vw] sm:w-[1264px]">
         <p className="flex items-center gap-x-1">Home <MdArrowForwardIos className="text-[#bf912d]" /> Author</p>
         {/* top */}
         <div className="">
           <AuthorCard />
         </div>
-        <div className="w-[837px] my-3 border bg-[#c5c5c5] h-1"></div>
+        <div className="w-[95vw] sm:w-[837px] my-3 border bg-[#c5c5c5] h-1"></div>
 
         <div className="flex justify-between gap-x-[36px]">
           {/* left */}
@@ -31,13 +31,12 @@ const AuthorContainer = () => {
 
         {/* bottom */}
         <div className="w-full sm:w-[837px] cursor-pointer">
-          <div>
-            <div className="rounded-[5px] h-[52px] border bg-[#bf912d] text-center mt-8 mb-14 text-[34px] flex justify-center">
-              <p className="text-[#ffd200]">MORE STORIES</p>
+            <div>
+              <div className="rounded-[5px] h-[40] sm:h-[52px] border bg-[#bf912d] text-center mt-8 mb-14 text-[23px] sm:text-[34px] flex justify-center">
+                <p className="text-[#ffd200]">MORE STORIES</p>
+              </div>
             </div>
           </div>
-        </div>
-
       </div>
     </div>
   )
